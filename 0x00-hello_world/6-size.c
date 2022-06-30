2f28c2d5de0be4ea8b3fead12e11f51c7086c4f9
Jehiel-MW <olami.solomon@gmail.com>
2022-06-30 15:33:42 -0700

    Programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: End of input
- */
+* main - Entry point
+* Return: End of input
+*/
 int main(void)
 {
-	char a;
-	int i;
-	long int li;
-	long long int lli;
-	float f;
+char a;
+int i;
+long int li;
+long long int lli;
+float f;
 
-	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
-	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
-	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	
-	return (0);
+return (0);
