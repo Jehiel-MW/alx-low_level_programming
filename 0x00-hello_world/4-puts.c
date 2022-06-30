7b2f5257e0d24399994443023ad3829e071f130f
Jehiel-MW <olami.solomon@gmail.com>
2022-06-30 11:12:13 -0700

    My programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+{
+	//string initialisation
+	char C_task_4[] = "Programming is like building a multilingual puzzle";
+	puts(C_task_4);
+	//writing the string to stdout
+	return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void);
+{
+	//this is not fun
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
