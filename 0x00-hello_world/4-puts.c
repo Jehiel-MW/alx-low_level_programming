f0b0d21686a7ac2a9d8166600ac414444336dae0
Jehiel-MW <olami.solomon@gmail.com>
2022-06-30 13:28:24 -0700

    Programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
-#include <stdio.h>                                int main(void)
+#include <stdio.h>
+int main(void)
 {
-        /**
-         * main - String initialisation                    *
-         * Return 0                                        */                                               puts("Programming is like building a multilingual puzzle\n");                                       return (0);                               }
+	/**
+	 * main - String initialisation
+	 *
+	 * Return 0
+	 */
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
