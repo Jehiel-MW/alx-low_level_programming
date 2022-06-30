649e0ffa73946fc8564d3f096745ca05ed6030d7
Jehiel-MW <olami.solomon@gmail.com>
2022-06-30 14:47:51 -0700

    This is tedious

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdio.h>
 /**
  * main - Entry point
  * Return: Program ending
@@ -11,10 +11,10 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("size of char: %lu byte(s) \n", (unsigned long)sizeof(a));
+	printf("size of a char: %lu byte(s) \n", (unsigned long)sizeof(a));
 	printf("size of an int: %lu byte(s) \n", (unsigned long)sizeof(i));
 	printf("size of a long int: %lu byte(s) \n", (unsigned long)sizeof(li));
 	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
 	printf("size of a float: %lu byte(s) \n", (unsigned long)sizeof(f));
-
-	return (0)
+	
+	return (0);
