621eeb4dd43252a1e05dcaee8133db94606d4d36
Jehiel-MW <olami.solomon@gmail.com>
2022-07-01 16:32:36 -0700

    last digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main -> assign a random number to the variable n each time it is execute
+* Return: always 0
+*/
+int main(void)
+{
+int i;
+for (i = 48 ; i <= 57 ; i++)
+{
+putchar(i);
+}
+putchar(10);
+
+return (0);
+}
