b1351dfc0f71151f9b58b99951c5c93f65562511
Jehiel-MW <olami.solomon@gmail.com>
2022-07-01 16:52:23 -0700

    Program of hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main -> assign a random number to the variable n each time it is execute
+* Return: always 0
+*/
+int main(void)
+{
+int i;
+for (i = 1 ; i <= 16 ; i++)
+{
+putchar(i);
+}
+putchar(10);
+return (0);
+}
