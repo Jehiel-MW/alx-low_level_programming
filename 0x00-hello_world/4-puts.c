c523593fc9777b4723cb0c744244eda1da902692
Jehiel-MW <olami.solomon@gmail.com>
2022-06-30 17:10:47 -0700

    programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,11 @@
 /**
  * main - Entry point
  *
- * Return 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
 	char items[] = "\"Programming is like building a multilingual puzzle";
-	
 	puts(items);
 	return (0);
 }
