03d789ebc62a68cd19382148116232c0858fed10
Jehiel-MW <olami.solomon@gmail.com>
2022-07-01 21:28:46 -0700

    Last comb

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,17 +1,38 @@
 #include <stdio.h>
+
 /**
-* main - Entry point
+* main - prints all possible different combinations of two digits
 *
-* Return: Always 0
+*Return: Always 0 (success)
 */
+
 int main(void)
 {
-int num;
-char letter;
-for (num = 0; num < 10; num++)
-putchar((num % 10) + '0');
-for (letter = 'a'; letter <= 'f'; letter++)
-putchar(letter);
-putchar('\n');
+int w, x, y, z;
+for (w = '0'; w <= '9'; w++)
+{
+for (x = '0'; x <= '9'; x++)
+{
+for (y = '0'; y <= '9'; y++)
+{
+for (z = '0'; z <= '9'; z++)
+{
+if (w < y || (w == y && x < z))
+{
+putchar(w);
+putchar(x);
+putchar(' ');
+putchar(y);
+putchar(z);
+if (w == '9' && x == '8' && y == '9' && z == '9')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar(10);
 return (0);
 }
