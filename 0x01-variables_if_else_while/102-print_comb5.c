bae0f8f9f491131bf5419bc20053100c850563ca
Jehiel-MW <olami.solomon@gmail.com>
2022-07-01 20:38:34 -0700

    last

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,18 +3,18 @@
 /**
 * main - prints all possible different combinations of two digits
 *
-*Return: 0
+*Return: Always 0 (success)
 */
 int main(void)
 {
 int w, x, y, z;
-for (w = 0 ; w <= 9 ; w++)
+for (w = '0' ; w <= '9' ; w++)
 {
-for (x = 0 ; x <= 9 ; x++)
+for (x = '0' ; x <= '9' ; x++)
 {
-for (y = 0 ; y <= 9 ; y++)
+for (y = '0' ; y <= '9' ; y++)
 {
-for (z = 0 ; z <= 9 ; z++)
+for (z = '0' ; z <= '9' ; z++)
 {
 if (w < y || (w == y && x < z))
 {
@@ -23,8 +23,8 @@ putchar(x);
 putchar(' ');
 putchar(y);
 putchar(z);
-if (w == 9 && x == 8 && y == 9 && z == 9)
-	continue;
+if (w == '9' && x == '8' && y == '9' && z == '9')
+continue;
 putchar(',');
 putchar(' ');
 }
@@ -32,7 +32,6 @@ putchar(' ');
 }
 }
 }
-putchar('\n');
+putchar(10);
 return (0);
-}
-	
+}	
