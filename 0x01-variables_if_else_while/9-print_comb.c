571a40215c09d9feb406ec9a98e7b780c60125e6
Jehiel-MW <olami.solomon@gmail.com>
2022-07-01 17:31:09 -0700

    Combing numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/**
+* main -> assign a random number to the variable n each time it is execute
+* Return: always 0
+*/
+int main(void)
+{
+int i;
+for (i = 48 ; i <= 57 ; i++)
+if (i != 57)
+{
+putchar(i);
+}
+{
+putchar(44);
+putchar(32);
+}
+putchar(10);
+return (0);
+}
