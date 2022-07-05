d81175fbaa53cc9f774b56814fedc7cbf46d1f97
Jehiel-MW <olami.solomon@gmail.com>
2022-07-05 11:42:38 -0700

    error hard to fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,6 +2,7 @@
 
 /**
 * print_alphabet -> print the alphabets from a-z
+* Return: always 0 (success)
 */
 
 void print_alphabet(void)
@@ -13,4 +14,5 @@ for (n = 'a' ; n <= 'z' ; n++)
 _putchar(n);
 }
 _putchar('\n');
+return (0);
 }
