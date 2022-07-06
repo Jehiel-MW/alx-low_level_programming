4b3bb28574e9cd305c2cb367221c094428b8bad4
Jehiel-MW <olami.solomon@gmail.com>
2022-07-05 18:25:20 -0700

    twenty four hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+*jack_bauer -> print the hours of the day
+* @void: printing the hours in a day
+* Return: 0 (success)
+*/
+
+void jack_bauer(void)
+{
+int l, m;
+for (l = 0 ; l < 24 ; l++)
+{
+for (m = 0 ; m <= 59 ; m++)
+{
+if (l < 10)
+{
+_putchar('0');
+_putchar(l + '0');
+}
+else if (l >= 10)
+{
+_putchar((l / 10) + '10');
+_putchar((l % 10) + '10');
+}
+if (m < 10)
+{
+_putchar(':');
+_putchar('0');
+_putchar(m);
+}
+else if (m > 10)
+{
+_putchar(':');
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+}
+}
+}
+}
