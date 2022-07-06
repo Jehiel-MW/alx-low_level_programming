2f2fef0c8682feedba05fcfaab73e8ee1b8c73f7
Jehiel-MW <olami.solomon@gmail.com>
2022-07-06 15:09:32 -0700

    multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,34 +8,35 @@
 
 void times_table(void)
 {
-int a, b, c, d, x;
-for (a = 0; a <= 9; a++)
+int r, c;
+int d, u;
+int mul;
+for (r = 0; r <= 9; r++)
 {
-for (b = 0; b <= 9; b++)
+for (c = 0; c <= 9; c++)
 {
-c = a * b;
-if (c > 9)
+mul = r * c;
+if (mul >= 10)
 {
-d = c % 10;
-x = (c - x) / 10;
-{
-_putchar(44);
-_putchar(32);
-_putchar(x + '0');
+d = mul / 10;
+u = mul % 10;
+_putchar(' ');
 _putchar(d + '0');
+_putchar(u + '0');
 }
 else
 {
-if (b != 0)
+if (c != 0)
 {
-_putchar(44);
-_putchar(32);
-_putchar(32);
+_putchar(' ');
+_putchar(' ');
 }
+_putchar(mul + '0');
 }
-_putchar(c + '0');
-}
-}
+if (c != 9)
+_putchar(',');
+if (c == 9)
 _putchar('\n');
+}
 }
 }
