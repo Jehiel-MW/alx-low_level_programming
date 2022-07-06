182f071ad8f988d96f03cb180cc1f9d1e4ce943d
Jehiel-MW <olami.solomon@gmail.com>
2022-07-06 15:28:38 -0700

    printing of numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,39 +5,41 @@
 * @n: The number to start printing from
 * Return: Always 0 (success)
 */
+
 void print_to_98(int n)
 {
-int n;
-if (n <= 98)
+int i;
+if (n > 98)
 {
-for (; n <= 98; n++)
+for (i = n; i >= 98; i--)
 {
-if (n == 98)
-{
-printf("%d", n);
+if (i != 98)
+printf("%d, ", i);
+else
+printf("%d", i);
+}
 printf("\n");
-break;
 }
+else if (n < 0 && n < 98)
+{
+for (i = n; i <= 98; i++)
+{
+if (i != 98)
+printf("%d, ", i);
 else
-{
-printf("%d, ", n);
+printf("%d", i);
 }
-}
-}
-else
-{
-for (; n >= 98; n--)
-{
-if (n == 98)
-{
-printf("%d", n);
 printf("\n");
-break;
 }
 else
 {
-printf("%d, ", n);
+for (i = n; i <= 98; i++)
+{
+if (i != 98)
+printf("%d, ", i);
+else
+printf("%d", i);
 }
-}
+printf("\n");
 }
 }
