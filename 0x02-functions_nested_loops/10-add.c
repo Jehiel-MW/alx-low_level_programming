bb8d32bf56f47ef1a272a5b2ffb86b9f18296e24
Jehiel-MW <olami.solomon@gmail.com>
2022-07-05 19:33:48 -0700

    addition of numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* add -> print the outcome of two added value
+	* @int: addition of numbers
+* Return: 0 (success)
+*/
+
+int add(int, int)
+{
+{
+int sum;
+sum = a + b;
+return (sum);
+}
+{
+int a;
+int b;
+int result;
+
+a = 5;
+b = 7;
+result = add('a', 'b');
+printf("%d\n", result);
+return (0);
+}
